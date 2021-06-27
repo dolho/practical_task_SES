cf65ed541174e37f6309407f4893dde675f9a534
Your Name <oleksii.dolhov@gmail.com>
2021-06-27 22:04:04 +0300

    fixed converter

--- app/blueprints/converter/converter.py
+++ app/blueprints/converter/converter.py
@@ -4,6 +4,7 @@ import threading
 import time
 import datetime
 import jwt
+import json
 
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -11,18 +12,21 @@ load_dotenv(find_dotenv())
 
 SECRET_SALT = os.environ['SECRET_SALT']
 
-RATE = [{}]
 
 
 def requests_thread():
-    global RATE
     while True:
         print('started to get info')
         r = requests.get('https://api.coinbase.com/v2/prices/BTC-UAH/buy')
         rate = r.json()['data']
         rate['time'] = datetime.datetime.now()
-        print(f'the rate is: {RATE}')
-        RATE[0] = rate.copy()
+        with open('rate.json', 'w') as f:
+            # It is needed to overwrite file
+            f.seek(0)
+            f.truncate()
+            json.dump(rate, f, default=str)
+        # print(f'the rate is: {RATE}')
+        # RATE[0] = rate.copy()
         time.sleep(60)
 
 x = threading.Thread(target=requests_thread)
@@ -61,5 +65,6 @@ def get_current_price():
         return Response("Bad request. Invalid token", status=400)
     if not payload['is_activated']:
         return Response("Unauthorized. Confirm your email first", status=401)
-    print('I will return rate: ', RATE[0])
-    return RATE[0]
+    with open('rate.json', 'r') as f:
+        data = json.load(f)
+    return data
